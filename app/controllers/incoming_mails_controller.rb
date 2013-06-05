b734796ad69ce778d64a71bae68e0b958c17dda3
Ted Haile <theodore.haile@gmail.com>
2013-06-05 16:10:25 -0700

    create new ticket from email

--- app/controllers/incoming_mails_controller.rb
+++ app/controllers/incoming_mails_controller.rb
@@ -7,6 +7,12 @@ class IncomingMailsController < ApplicationController
     Rails.logger.log Logger::INFO, message.subject #print the subject to the logs
     Rails.logger.log Logger::INFO, message.body.decoded #print the decoded body to the logs
 
+    @ticket = Ticket.new
+    @ticket.name = message.subject
+    @ticket.content = message.body.decoded
+
+    @ticket.save
+
     # Do some other stuff with the mail message
 
     render :text => 'success', :status => 200 # a status of 404 would reject the mail
