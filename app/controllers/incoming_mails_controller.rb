d34e87c3065248462482177f1343c8bee9d6bb58
Ted Haile <theodore.haile@gmail.com>
2013-06-05 16:29:55 -0700

    added headers to subject

--- app/controllers/incoming_mails_controller.rb
+++ app/controllers/incoming_mails_controller.rb
@@ -8,9 +8,9 @@ class IncomingMailsController < ApplicationController
     # Rails.logger.log Logger::INFO, message.body.decoded #print the decoded body to the logs
 
     @ticket = Ticket.new
-    @ticket.name = params[:Subject]
+    @ticket.name = params[:headers][:subject]
     @ticket.content = params[:plain]
-    @ticket.user = params[:From]
+    @ticket.user = params[:from]
     @ticket.save
 
     # Do some other stuff with the mail message
