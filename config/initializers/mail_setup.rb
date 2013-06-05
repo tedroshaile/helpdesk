97157becdce8928a4091382fcda7ea2efd9582a0
Ted Haile <theodore.haile@gmail.com>
2013-06-05 15:40:37 -0700

    another typo

--- config/initializers/mail_setup.rb
+++ config/initializers/mail_setup.rb
@@ -1,6 +1,6 @@
 ActionMailer::Base.smtp_settings = {
   :address => "smtp.sendgrid.net",
-  :port => ’587′,
+  :port => "587",
   :authentication => :plain,
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
